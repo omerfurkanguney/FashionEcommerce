3889504cb1ba0717a935c8040d0c5d8326a41bde
Ömer Güney <furkanguney3408@gmail.com>
2022-03-28 21:08:31 +0300

    City(CRUD) operasyonları eklendi.

---
+++ Business/Constants/CityMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class CityMessages
+    {
+        public static string CityAdded = "Şehir Eklendi";
+        public static string CityDeleted = "Şehir Silindi";
+        public static string CityListed = "Şehir Listelendi";
+        public static string CityUpdated = "Şehir Güncellendi";
+    }
+}
---
+++ WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,72 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        ICityService _cityService;
+        public CitiesController(ICityService cityService)
+        {
+            _cityService = cityService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _cityService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);  
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int cityId)
+        {
+            var result = _cityService.GetById(cityId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(City city)
+        {
+            var result = _cityService.Add(city);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(City city)
+        {
+            var result = _cityService.Delete(city);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(City city)
+        {
+            var result = _cityService.Update(city);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
